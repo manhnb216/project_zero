5b063c7c14f371e2316c7c21c5b9bfc37df226c1
mahn <manhnb216@gmail.com>
2022-06-06 11:01:30 +0700

    resolve issue

--- lib/presentation/page/home/home_state.dart
+++ lib/presentation/page/home/home_state.dart
@@ -1,15 +1,18 @@
 import 'package:de1_mobile_friends/domain/model/food.dart';
 import 'package:de1_mobile_friends/tuAI/tu_ai.dart';
+import 'package:flutter/material.dart';
 
 abstract class HomeState {
   final List<Food>? foods;
   final Food? pickedFood;
+  final Food? foodResultTemp;
   final TuAiOutput? tuAiOutput;
 
   HomeState({
     this.foods,
     this.pickedFood,
     this.tuAiOutput,
+    this.foodResultTemp
   });
 }
 
@@ -20,16 +23,19 @@ class HomePrimaryState extends HomeState {
     List<Food>? foods,
     Food? pickedFood,
     TuAiOutput? tuAiOutput,
-  }) : super(foods: foods, pickedFood: pickedFood, tuAiOutput: tuAiOutput);
+    Food? foodResultTemp
+  }) : super(foods: foods, pickedFood: pickedFood, tuAiOutput: tuAiOutput, foodResultTemp: foodResultTemp);
 
   HomePrimaryState copyWith({
     List<Food>? foods,
     Food? pickedFood,
     TuAiOutput? tuAiOutput,
+    Food? foodResultTemp
   }) =>
       HomePrimaryState(
         foods: foods ?? this.foods,
         pickedFood: pickedFood ?? this.pickedFood,
         tuAiOutput: tuAiOutput ?? this.tuAiOutput,
+        foodResultTemp: foodResultTemp ?? this.foodResultTemp
       );
 }
